e8552a9106ff54df2707fdcad2f41f47f1cadb0e
kneauxn <kneauxn@gmail.com>
2016-11-10 23:32:21 -0700

    Created the initial file for the 301 game type. Haven't got much yet.

---
+++ 301.js
@@ -0,0 +1,26 @@
+// Game mode 301
+console.log("Initiating new game of 301");
+console.log('Player 1 Score: 301 \nPlayer 2 Score: 301');
+console.log("Player 1 Now Throwing");
+
+var playerScore1 = 301;
+var platerScore2 = 301;
+
+// Determining the value of the current throw
+// This switch translates each throw into a point value
+
+
+// This Part Just sums the values determined by the previous switch
+funtion throwSum(points){
+var throwVal = 0;
+for (var i = 0; i < pointArray.length; i ++) {
+    sum += parseInt(pointArray[i]);
+    }
+}
+};
+
+// for the purpose of testing we will handle these values through process arguments
+pointArray = process.argv.splice(0,2)
+console.log(pointArray);
+console.log(throwSum(pointArray))
+

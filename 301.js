8a8e392ce5138d5a4affd23dc02654a441036bec
kneauxn <kneauxn@gmail.com>
2016-11-11 19:56:15 -0700

    Got the multipliers working for dubs and trips.

--- 301.js
+++ 301.js
@@ -4,23 +4,19 @@ console.log('Player 1 Score: 301 \nPlayer 2 Score: 301');
 console.log("Player 1 Now Throwing");
 
 var playerScore1 = 301;
-var platerScore2 = 301;
+var currentThrow = "T10";
+var throwInt = parseInt(currentThrow.slice(1));
+var throwVal;
 
-// Determining the value of the current throw
-// This switch translates each throw into a point value
-
-
-// This Part Just sums the values determined by the previous switch
-funtion throwSum(points){
-var throwVal = 0;
-for (var i = 0; i < pointArray.length; i ++) {
-    sum += parseInt(pointArray[i]);
+// is the current throw multiplied by any value?
+multiplier = function() {
+    if (currentThrow.charAt(0) == "D") {
+        throwVal = throwInt * 2;
+    } else {
+        if (currentThrow.charAt(0) == "T") {
+            throwVal = throwInt * 3;
+        }
     }
-}
 };
-
-// for the purpose of testing we will handle these values through process arguments
-pointArray = process.argv.splice(0,2)
-console.log(pointArray);
-console.log(throwSum(pointArray))
-
+multiplier();
+console.log(throwVal)
